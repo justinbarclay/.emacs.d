fd3c3884c61cf5761cb49aaa969f5c4bcb60c548
Justin Barclay <justincbarclay@gmail.com>
2017-01-11 08:34:44 -0700

    Change shell integration from window based to system based, now works when running from daemon

--- customizations/shell-integration.el
+++ customizations/shell-integration.el
@@ -1,7 +1,7 @@
 ;; Sets up exec-path-from shell
 ;; https://github.com/purcell/exec-path-from-shell
 
-(when (memq window-system '(mac ns))
+(when (memq system-type '(darwin))
   (exec-path-from-shell-initialize)
   (exec-path-from-shell-copy-envs
    '("PATH")))
