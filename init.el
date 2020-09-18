fc24d89c336588e953970559b0448e5e6dd0cdef
Justin Barclay <justincbarclay@gmail.com>
2020-09-17 23:43:50 -0600

    Update elegant-agenda from function to mode

--- init.el
+++ init.el
@@ -220,32 +220,56 @@ PRIORITY may be one of the characters ?A, ?B, or ?C."
                               (air-org-skip-subtree-if-priority ?A)))
                         (org-agenda-overriding-header "Tasks:"))))))))
 
-(defun elegant-agenda (orig-func &rest args)
-  ""
-  (interactive)
+(defvar elegant-agenda-transforms nil)
+
+(defvar elegant-agenda-face-remappings (let ((face-height (face-attribute 'default :height)))
+                                         (list
+                                          (list 'default (list :family "Yanone Kaffeesatz Light"
+                                                               :height (ceiling (* face-height 1.5)) :weight 'thin))
+                                          (list 'header-line (list :family "Yanone Kaffeesatz Light"
+                                                                   :height (* face-height 2) :weight 'thin
+                                                                   :underline nil  :overline nil :box nil))                                          
+                                          (list 'bold (list :height (ceiling (* face-height 1.1)) :weight 'light))
+                                          '(italic (:foreground "orange"))
+                                          '(org-link (:foreground "white"))))
+  "A list of faces and the associated specs that will be remapped when elegant-agenda-mode is enabled")
+
+(defun elegant-agenda--enable ()
   (let ((width (window-width))
-        (title "—  T O D A Y  ")
-        (org-agenda-use-time-grid nil)
-        (line-spacing 8))
+        (title "—  T O D A Y  "))
     (setq-local line-spacing 8)
-    (setq org-agenda-block-separator "  ")    
-    (apply orig-func args)
+    (setq-local org-agenda-use-time-grid nil)
+    (setq-local org-agenda-block-separator "  ")
     (display-line-numbers-mode 0)
-    (face-remap-add-relative
-     'header-line '(:family "Yanone Kaffeesatz Light"
-                    :height 2.0 :weight thin
-                    :foreground "#000000" :background "#ffffff"
-                    :underline nil  :overline nil :box nil))
-    (face-remap-add-relative 'default '(:family "Yanone Kaffeesatz Light"
-                                        :height 1.5 :weight thin))
-    (face-remap-add-relative 'italic '(:foreground "orange"))
-    (face-remap-add-relative 'bold '(:height 1.1 :weight light))
-    (face-remap-add-relative 'org-link '(:foreground "white"))
+    (setq elegant-agenda-transforms
+          (mapcar (lambda (face-&-spec)
+                    (face-remap-add-relative (car face-&-spec) (cadr face-&-spec)))
+                  elegant-agenda-face-remappings))
     (setq-local mode-line-format nil)
     (setq-local header-line-format
                 (format "%s%s" title (make-string (- width (length title)) ?— t)))))
 
-(advice-add 'org-agenda :around #'elegant-agenda)
+(defun elegant-agenda--disable ()
+  (setq-local line-spacing (default-value 'line-spacing))
+  (setq-local org-agenda-use-time-grid (default-value 'line-spacing))
+  (setq-local org-agenda-block-separator (default-value 'org-agenda-block-separator))    
+
+  (mapc #'face-remap-remove-relative
+        elegant-agenda-transforms)     
+  (setq-local elegant-agenda-transforms nil)
+
+  (setq-local mode-line-format (default-value 'mode-line-format))
+  (setq-local header-line-format nil))
+
+(define-minor-mode elegant-agenda-mode
+  "Provides a more elegant view into your agenda"
+  :init-value nil :lighter " elegant-agenda" :keymap nil
+  (if elegant-agenda-mode
+      (elegant-agenda--enable)
+    (elegant-agenda--disable))
+  (force-window-update (current-buffer)))
+
+(add-hook 'org-agenda-mode-hook #'elegant-agenda-mode)
 (setq initial-buffer-choice (lambda () (org-agenda nil "d")
                                        (buffer-find "*Org Agenda*")))
 
