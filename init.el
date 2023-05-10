5f81df0c07a91f5c63ca5ac4653a834f7259e0c5
Justin Barclay <git@justinbarclay.ca>
2023-05-10 09:30:42 -0700

    Switch out straight for elpaca

--- init.el
+++ init.el
@@ -1,4 +1,3 @@
-(package-initialize)
 (setq native-comp-deferred-compilation-deny-list '())
 (setq native-comp-async-report-warnings-errors nil)
 
@@ -12,18 +11,50 @@
 (setq user-full-name "Justin Barclay"
       user-mail-address "me@justincbarclay.ca")
 
-(defvar bootstrap-version)
-(let ((bootstrap-file
-       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
-       (bootstrap-version 5))
-  (unless (file-exists-p bootstrap-file)
-    (with-current-buffer
-        (url-retrieve-synchronously
-         "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
-         'silent 'inhibit-cookies)
-      (goto-char (point-max))
-      (eval-print-last-sexp)))
-  (load bootstrap-file nil 'nomessage))
+(defvar elpaca-installer-version 0.4)
+(defvar elpaca-directory (expand-file-name "elpaca/" user-emacs-directory))
+(defvar elpaca-builds-directory (expand-file-name "builds/" elpaca-directory))
+(defvar elpaca-repos-directory (expand-file-name "repos/" elpaca-directory))
+(defvar elpaca-order '(elpaca :repo "https://github.com/progfolio/elpaca.git"
+                              :ref nil
+                              :files (:defaults (:exclude "extensions"))
+                              :build (:not elpaca--activate-package)))
+(let* ((repo  (expand-file-name "elpaca/" elpaca-repos-directory))
+       (build (expand-file-name "elpaca/" elpaca-builds-directory))
+       (order (cdr elpaca-order))
+       (default-directory repo))
+  (add-to-list 'load-path (if (file-exists-p build) build repo))
+  (unless (file-exists-p repo)
+    (make-directory repo t)
+    (when (< emacs-major-version 28) (require 'subr-x))
+    (condition-case-unless-debug err
+        (if-let ((buffer (pop-to-buffer-same-window "*elpaca-bootstrap*"))
+                 ((zerop (call-process "git" nil buffer t "clone"
+                                       (plist-get order :repo) repo)))
+                 ((zerop (call-process "git" nil buffer t "checkout"
+                                       (or (plist-get order :ref) "--"))))
+                 (emacs (concat invocation-directory invocation-name))
+                 ((zerop (call-process emacs nil buffer nil "-Q" "-L" "." "--batch"
+                                       "--eval" "(byte-recompile-directory \".\" 0 'force)")))
+                 ((require 'elpaca))
+                 ((elpaca-generate-autoloads "elpaca" repo)))
+            (kill-buffer buffer)
+          (error "%s" (with-current-buffer buffer (buffer-string))))
+      ((error) (warn "%s" err) (delete-directory repo 'recursive))))
+  (unless (require 'elpaca-autoloads nil t)
+    (require 'elpaca)
+    (elpaca-generate-autoloads "elpaca" repo)
+    (load "./elpaca-autoloads")))
+(add-hook 'after-init-hook #'elpaca-process-queues)
+(elpaca `(,@elpaca-order))
+
+;; Install use-package support
+(elpaca elpaca-use-package
+  ;; Enable :elpaca use-package keyword.
+  (elpaca-use-package-mode))
+(elpaca-wait)
+
+(package-initialize)
 
 (setq use-package-compute-statistics t)
 (setq use-package-minimum-reported-time 0.01)
@@ -477,7 +508,7 @@ PRIORITY may be one of the characters ?A, ?B, or ?C."
   (display-line-numbers-mode -1))
 
 (use-package mu4e-dashboard
-  :straight (:type git :host github :repo "rougier/mu4e-dashboard")
+  :elpaca (:type git :host github :repo "rougier/mu4e-dashboard")
   :bind ("C-c d" . mu4e-dashboard)
   :hook
   (mu4e-dashboard-mode . (lambda () (display-line-numbers-mode -1)))
@@ -489,7 +520,7 @@ PRIORITY may be one of the characters ?A, ?B, or ?C."
 
 (use-package mu4e-thread-folding
   :after mu4e
-  :straight (:type git :host github :repo "rougier/mu4e-thread-folding")
+  :elpaca (:type git :host github :repo "rougier/mu4e-thread-folding")
   :init
   (add-to-list 'mu4e-header-info-custom
                '(:empty . (:name "Empty"
@@ -560,10 +591,10 @@ PRIORITY may be one of the characters ?A, ?B, or ?C."
 (setq read-process-output-max (* 1024 1024))
 
 (use-package nano-theme
-  :straight (nano-theme :type git :host github :repo "rougier/nano-theme"))
+  :elpaca (nano-theme :type git :host github :repo "rougier/nano-theme"))
 
 (use-package lambda-themes
-  :straight (:type git :host github :repo "lambda-emacs/lambda-themes") 
+  :elpaca (:type git :host github :repo "lambda-emacs/lambda-themes") 
   :custom
   (lambda-themes-set-italic-comments nil)
   (lambda-themes-set-italic-keywords nil)
@@ -573,10 +604,10 @@ PRIORITY may be one of the characters ?A, ?B, or ?C."
   (load-theme 'lambda-light))
 
 (use-package doom-everblush-theme
-  :straight (doom-everblush-theme :type git :host github :repo "Everblush/doomemacs"))
+  :elpaca (doom-everblush-theme :type git :host github :repo "Everblush/doomemacs"))
 
 (use-package doom-themes
-  :defer 1
+  :elpaca t
   :init
   (load-theme 'doom-everblush t) 
   ;; When using doom-themes region is a much better colour for highlighting current line
@@ -1257,7 +1288,7 @@ parses its input."
   :after yasnippet)
 
 (use-package yasnippet-capf
-  :straight (:type git :host github :repo "justinbarclay/yasnippet-capf"))
+  :elpaca (:type git :host github :repo "justinbarclay/yasnippet-capf"))
 
 (use-package treesit
   :ensure nil
@@ -1274,9 +1305,18 @@ parses its input."
   (global-treesit-auto-mode))
 
 (use-package combobulate
-  :straight (:type git :host github :repo "mickeynp/combobulate")
+  :elpaca (:type git :host github :repo "mickeynp/combobulate")
   :hook (prog-mode . combobulate-mode)
   :custom (combobulate-js-ts-enable-auto-close-tag . nil))
+
+(use-package copilot
+  :elpaca (:host github :repo "zerolfx/copilot.el" :files ("dist" "*.el"))
+  :hook (prog-mode . copilot-mode)
+  :bind (:map copilot-completion-map
+              ("<tab>" . 'copilot-accept-completion)
+              ("TAB" . 'copilot-accept-completion)
+              ("C-TAB" . 'copilot-accept-completion-by-word)
+              ("C-<tab>" . 'copilot-accept-completion-by-word)))
 
 (use-package lsp-mode
   :commands lsp
@@ -1298,8 +1338,8 @@ parses its input."
         lsp-log-io nil
         lsp-completion-provider :none
         lsp-headerline-breadcrumb-enable nil
-        lsp-solargraph-use-bundler 't
-        lsp-use-plists 't)
+        lsp-solargraph-use-bundler 't)
+        ;;lsp-use-plists 't)
   :init
   (defun my/orderless-dispatch-flex-first (_pattern index _total)
     (and (eq index 0) 'orderless-flex))
