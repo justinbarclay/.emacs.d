8996dfbe1cd8293ce36b60ca366517a8c6fabe50
Justin Barclay <git@justinbarclay.ca>
2024-07-31 01:17:51 -0700

    chore: upgrade company-box with nerd-icons
    Acked-by: Justin Barclay <git@justinbarclay.ca>

--- init.el
+++ init.el
@@ -683,9 +683,7 @@ PRIORITY may be one of the characters ?A, ?B, or ?C."
 
 (use-package nerd-icons-completion
   :after (nerd-icons marginalia)
-  :hook (marginalia-mode . nerd-icons-completion-marginalia-setup)
-  :config
-  (nerd-icons-completion-mode))
+  :hook (marginalia-mode . nerd-icons-completion-marginalia-setup))
 
 (use-package nerd-icons-ibuffer
   :hook (ibuffer-mode . nerd-icons-ibuffer-mode))
@@ -1439,41 +1437,112 @@ parses its input."
   ;;; Set your shorthand favorite interactive command
   (setq consult-omni-default-interactive-command #'consult-omni-multi))
 
-(use-package cape
-  :init
-  ;; Add `completion-at-point-functions', used by `completion-at-point'.
-  (add-to-list 'completion-at-point-functions #'cape-dabbrev)
-  (add-to-list 'completion-at-point-functions #'cape-file)
-  (add-to-list 'completion-at-point-functions #'cape-dict))
-
 (use-package company
-  :commands (company-manual-begin)
-  :hook (elpaca-after-init . global-company-mode)
+  :commands (company-complete-common
+             company-complete-common-or-cycle
+             company-manual-begin
+             company-grab-line)
+  :hook ((elpaca-after-init . global-company-mode)
+         (company-mode . company-tng-mode))
   :bind (("C-<tab>" . company-manual-begin)
          :map company-active-map
          ("C->" . #'company-filter-candidates)
          ("C-/" . #'company-other-backend))
   :config
-  (setq company-tooltip-align-annotations t
-        company-minimum-prefix-length 1
+  (require 'company-files)
+  (setq company-minimum-prefix-length 1
+        company-tooltip-limit 14
         company-idle-delay 0.1
         company-selection-wrap-around t
-        company-tooltip-limit 10
-        company-backends '((company-capf :with company-yasnippet)
-                           company-yasnippet)
-        company-frontends '(company-pseudo-tooltip-unless-just-one-frontend-with-delay
-                            company-preview-frontend
-                            company-echo-metadata-frontend)))
+        company-tooltip-align-annotations t
+        company-require-match 'never
+        company-global-modes '(not erc-mode
+                                   circe-mode
+                                   message-mode
+                                   help-mode
+                                   gud-mode
+                                   vterm-mode)
+        company-frontends '(company-pseudo-tooltip-frontend  ; always show candidates in overlay tooltip
+                            company-echo-metadata-frontend)  ; show selected candidate docs in echo area
 
-;; (setq company-backends '((company-capf)
-;;                          company-yasnippet
-;;                          (company-dabbrev :with company-dabbrev-code company-keywords) 
-;;                          company-files))
+      ;; Buffer-local backends will be computed when loading a major mode, so
+      ;; only specify a global default here.
+      company-backends '((company-capf)
+                         company-yasnippet)
+
+      ;; These auto-complete the current selection when
+      ;; `company-auto-commit-chars' is typed. This is too magical. We
+      ;; already have the much more explicit RET and TAB.
+      company-auto-commit nil
+
+      ;; Only search the current buffer for `company-dabbrev' (a backend that
+      ;; suggests text your open buffers). This prevents Company from causing
+      ;; lag once you have a lot of buffers open.
+      ;; company-dabbrev-other-buffers nil
+      ;; Make `company-dabbrev' fully case-sensitive, to improve UX with
+      ;; domain-specific words with particular casing.
+      company-dabbrev-ignore-case nil
+      company-dabbrev-downcase nil)
+ (add-to-list 'company-files--regexps "file:\\(\\(?:\\.\\{1,2\\}/\\|~/\\|/\\)[^\]\n]*\\)"))
 
 (use-package company-box
   :hook (company-mode . company-box-mode)
+  :init
+  (require 'nerd-icons)
+  (setq company-box-icons-alist 'company-box-icons-nerd-icons
+        company-box-icons-nerd-icons
+      `((Unknown        . ,(nerd-icons-codicon  "nf-cod-code"                :face  'font-lock-warning-face))
+        (Text           . ,(nerd-icons-codicon  "nf-cod-text_size"           :face  'font-lock-doc-face))
+        (Method         . ,(nerd-icons-codicon  "nf-cod-symbol_method"       :face  'font-lock-function-name-face))
+        (Function       . ,(nerd-icons-codicon  "nf-cod-symbol_method"       :face  'font-lock-function-name-face))
+        (Constructor    . ,(nerd-icons-codicon  "nf-cod-triangle_right"      :face  'font-lock-function-name-face))
+        (Field          . ,(nerd-icons-codicon  "nf-cod-symbol_field"        :face  'font-lock-variable-name-face))
+        (Variable       . ,(nerd-icons-codicon  "nf-cod-symbol_variable"     :face  'font-lock-variable-name-face))
+        (Class          . ,(nerd-icons-codicon  "nf-cod-symbol_class"        :face  'font-lock-type-face))
+        (Interface      . ,(nerd-icons-codicon  "nf-cod-symbol_interface"    :face  'font-lock-type-face))
+        (Module         . ,(nerd-icons-codicon  "nf-cod-file_submodule"      :face  'font-lock-preprocessor-face))
+        (Property       . ,(nerd-icons-codicon  "nf-cod-symbol_property"     :face  'font-lock-variable-name-face))
+        (Unit           . ,(nerd-icons-codicon  "nf-cod-symbol_ruler"        :face  'font-lock-constant-face))
+        (Value          . ,(nerd-icons-codicon  "nf-cod-symbol_field"        :face  'font-lock-builtin-face))
+        (Enum           . ,(nerd-icons-codicon  "nf-cod-symbol_enum"         :face  'font-lock-builtin-face))
+        (Keyword        . ,(nerd-icons-codicon  "nf-cod-symbol_keyword"      :face  'font-lock-keyword-face))
+        (Snippet        . ,(nerd-icons-codicon  "nf-cod-notebook_template"      :face  'font-lock-string-face))
+        (Color          . ,(nerd-icons-codicon  "nf-cod-symbol_color"        :face  'success))
+        (File           . ,(nerd-icons-codicon  "nf-cod-symbol_file"         :face  'font-lock-string-face))
+        (Reference      . ,(nerd-icons-codicon  "nf-cod-references"          :face  'font-lock-variable-name-face))
+        (Folder         . ,(nerd-icons-codicon  "nf-cod-folder"              :face  'font-lock-variable-name-face))
+        (EnumMember     . ,(nerd-icons-codicon  "nf-cod-symbol_enum_member"  :face  'font-lock-builtin-face))
+        (Constant       . ,(nerd-icons-codicon  "nf-cod-symbol_constant"     :face  'font-lock-constant-face))
+        (Struct         . ,(nerd-icons-codicon  "nf-cod-symbol_structure"    :face  'font-lock-variable-name-face))
+        (Event          . ,(nerd-icons-codicon  "nf-cod-symbol_event"        :face  'font-lock-warning-face))
+        (Operator       . ,(nerd-icons-codicon  "nf-cod-symbol_operator"     :face  'font-lock-comment-delimiter-face))
+        (TypeParameter  . ,(nerd-icons-codicon  "nf-cod-list_unordered"      :face  'font-lock-type-face))
+        (Template       . ,(nerd-icons-codicon  "nf-cod-notebook_template"      :face  'font-lock-escape-face))
+        (ElispFunction  . ,(nerd-icons-codicon  "nf-cod-symbol_method"       :face  'font-lock-function-name-face))
+        (ElispVariable  . ,(nerd-icons-codicon  "nf-cod-symbol_variable"     :face  'font-lock-variable-name-face))
+        (ElispFeature   . ,(nerd-icons-codicon  "nf-cod-globe"               :face  'font-lock-builtin-face))
+        (ElispFace      . ,(nerd-icons-codicon  "nf-cod-symbol_color"        :face  'success))))
   :config
-  (setq company-box-scrollbar nil))
+  (setq company-box-show-single-candidate t
+        company-box-backends-colors nil
+        company-box-tooltip-limit 50
+         ;; Move company-box-icons--elisp to the end, because it has a catch-all
+         ;; clause that ruins icons from other backends in elisp buffers.
+         company-box-icons-functions
+         (cons #'+company-box-icons--elisp-fn
+               (delq 'company-box-icons--elisp
+                     company-box-icons-functions)))
+   
+        
+  (setq company-box-scrollbar nil)
+
+  (defun +company-box-icons--elisp-fn (candidate)
+    (when (derived-mode-p 'emacs-lisp-mode)
+      (let ((sym (intern candidate)))
+        (cond ((fboundp sym)  'ElispFunction)
+              ((boundp sym)   'ElispVariable)
+              ((featurep sym) 'ElispFeature)
+              ((facep sym)    'ElispFace))))))
 
 (use-package yasnippet
  :init (yas-global-mode))
@@ -1537,9 +1606,10 @@ parses its input."
           tsx-ts-mode
           go-base-mode
           LaTeX-mode
+          nix-mode
           org-mode)
          . lsp-deferred)
-  (lsp-completion-mode . my/lsp-mode-setup-completion)
+         (lsp-completion-mode . my/lsp-mode-setup-completion)
   (lsp-mode . lsp-enable-which-key-integration)
   :custom
   (lsp-idle-delay 0.1)
