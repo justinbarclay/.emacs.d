02bd86e92621d2ae1c247bb34041b5a340ea2403
Justin Barclay <git@justinbarclay.ca>
2025-04-09 23:42:29 -0700

    feat: switch whitespace packages

--- init.el
+++ init.el
@@ -1015,9 +1015,28 @@ PRIORITY may be one of the characters ?A, ?B, or ?C."
 (use-feature smartparens-config
   :after smartparens)
 
-(use-package ws-butler
-  :commands (ws-butler-mode)
-  :hook (prog-mode . ws-butler-mode))
+(use-package stripspace
+  :ensure t
+
+  ;; Enable for prog-mode-hook, text-mode-hook, conf-mode-hook
+  :hook ((prog-mode . stripspace-local-mode)
+         (text-mode . stripspace-local-mode)
+         (conf-mode . stripspace-local-mode))
+
+  :custom
+  ;; The `stripspace-only-if-initially-clean' option:
+  ;; - nil to always delete trailing whitespace.
+  ;; - Non-nil to only delete whitespace when the buffer is clean initially.
+  ;; (The initial cleanliness check is performed when `stripspace-local-mode'
+  ;; is enabled.)
+  (stripspace-only-if-initially-clean nil)
+
+  ;; Enabling `stripspace-restore-column' preserves the cursor's column position
+  ;; even after stripping spaces. This is useful in scenarios where you add
+  ;; extra spaces and then save the file. Although the spaces are removed in the
+  ;; saved file, the cursor remains in the same position, ensuring a consistent
+  ;; editing experience without affecting cursor placement.
+  (stripspace-restore-column t))
 
 (use-package origami
   :defer t
